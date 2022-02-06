c0daded58f6bf9cf0dd409c44ca3d7b8a5e4c316
Christian_Gruenewald <christian.gruenewald@ifnano.de>
2022-02-06 01:29:36 +0100

    test download pics

--- webscraping.py
+++ webscraping.py
@@ -1,1 +1,21 @@
-print("test")
+# Data mining Test for Data science test:
+
+import requests 
+from bs4 import BeautifulSoup as bs
+import os
+
+
+url='https://the-morpheus.de/'
+soup=bs(requests.get(url).content, "html.parser")
+img_urls=[]
+
+for img in soup.find_all("img"):
+    img_url=(url + img.attrs.get("src"))
+    img_urls.append(img_url)
+
+for url in img_urls:
+    response=requests.get(url)
+    size=int(response.headers.get("Content-lenght",0))
+    name=os.path.join("img", url.split("/")[-1])
+    with open(name,"wb") as f_out:
+        f_out.write(response.content)
